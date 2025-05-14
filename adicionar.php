a35e5213093d1401641d0173015f77a69d018822
zwiiny <165833104+zwiiny@users.noreply.github.com>
2025-05-13 21:52:12 -0300

    Add files via upload

---
+++ adicionar.php
@@ -0,0 +1,8 @@
+<h2>Adicionar Novo Cliente</h2>
+<form action="inserir.php" method="post">
+  Nome: <input type="text" name="nome" required><br>
+  Telefone: <input type="text" name="telefone"><br>
+  Email: <input type="email" name="email"><br>
+  <button type="submit">Salvar</button>
+</form>
+<a href="index.php">Voltar</a>
